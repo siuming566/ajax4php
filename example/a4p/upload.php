428096f5399cf1d2c88669ac561948ef92e110c3
Siu Ming <siu@sasolution.com.hk>
2013-01-24 21:18:09 +0800

    - Added File Upload

---
+++ example/a4p/upload.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "security.inc.php";
+
+$yesterday = strtotime('-1 days');
+foreach (glob(session_save_path() . DIRECTORY_SEPARATOR . "upload_*") as $oldfile) {
+	if (filemtime($oldfile) < $yesterday)
+		unlink($oldfile);
+}
+
+$filename = session_save_path() . DIRECTORY_SEPARATOR . "upload_" . a4p_sec::randomString(32);
+move_uploaded_file($_FILES["file"]["tmp_name"], $filename);
+$_FILES["file"]["tmp_name"] = $filename;
+
+echo "@" . json_encode($_FILES["file"]);
