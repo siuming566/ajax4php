eafc8795c5fa5d14ceca4d7fc7cee24335cede0a
Siu Ming <siu@sasolution.com.hk>
2013-12-15 19:04:45 +0800

    array query distinct groupby count sum

--- example/a4p/arr.inc.php
+++ example/a4p/arr.inc.php
@@ -9,6 +9,7 @@ class arr
 	private $from = array();
 	private $where = "";
 	private $orderby = array();
+	private $groupby = array();
 	private $sort = "";
 
 	public function __construct($select) {
@@ -33,6 +34,11 @@ class arr
 	
 	public function orderby() {
 		$this->orderby = func_get_args();
+		return $this;
+	}
+
+	public function groupby() {
+		$this->groupby = func_get_args();
 		return $this;
 	}
 
@@ -62,12 +68,18 @@ class arr
 		return 0;
 	}
 
-	public function takeAll() {
-		return $this->take(-1);
+	public function _cmp2($a, $b) {
+		foreach($this->groupby as $prop) {
+			if ($a->$prop > $b->$prop)
+				return 1;
+			if ($a->$prop < $b->$prop)
+				return -1;
+		}
+		return 0;
 	}
 
-	public function take($n) {
-		$filter = array();
+	private function _filter() {
+		$match = array();
 		if (strlen($this->where) > 5) {
 			$regex = '/{(\w+)}/';
 			$where = preg_replace($regex, '$a->$1', substr($this->where, 5));
@@ -76,30 +88,39 @@ class arr
 			foreach ($this->from as $row) {
 				$obj = is_array($row) ? (object) $row : $row;
 				if ($condition($obj))
-					$filter[] = $obj;
+					$match[] = $obj;
 			}
 		} else {
 			foreach ($this->from as $obj)
-				$filter[] = $obj;
+				$match[] = $obj;
 		}
+		return $match;
+	}
+
+	public function takeAll() {
+		return $this->take(-1);
+	}
+
+	public function take($n) {
+		$match = $this->_filter();
 
 		if (count($this->orderby) > 0)
-			usort($filter, array($this, "_cmp"));
+			usort($match, array($this, "_cmp"));
 
 		if (strtolower($this->sort) == "asc")
-			sort($filter);
+			sort($match);
 
 		if (strtolower($this->sort) == "desc")
-			rsort($filter);
+			rsort($match);
 
 		if ($n == -1)
-			$n = count($filter);
+			$n = count($match);
 
 		if (count($this->select) == 1) {
 			$prop = $this->select[0];
 			$result = array();
 			$i = 1;
-			foreach ($filter as $row) {
+			foreach ($match as $row) {
 				$result[] = $row->$prop;
 				if ($i++ >= $n)
 					break;
@@ -108,7 +129,7 @@ class arr
 		} else if (count($this->select) > 0) {
 			$result = array();
 			$i = 1;
-			foreach ($filter as $row) {
+			foreach ($match as $row) {
 				$obj = new stdclass();
 				foreach ($this->select as $prop) 
 					$obj->$prop = $row->$prop;
@@ -118,6 +139,112 @@ class arr
 			}
 			return $result;
 		} else
-			return array_slice($filter, 0, $n);
+			return array_slice($match, 0, $n);
+	}
+
+	public function each($func) {
+		$arr = $this->takeAll();
+		$result = array();
+		foreach ($arr as $obj)
+			$result[] = $func($obj);
+		return $result;
+	}
+
+	public function distinct($prop = null) {
+		$match = $this->_filter();
+
+		if ($prop != null) {
+			$result = array();
+			foreach ($match as $row)
+				$result[] = $row->$prop;
+		}
+		else
+			$result = $match;
+
+		return array_values(array_unique($result));
+	}
+
+	private function reset_match() {
+		$match = new stdclass();
+		foreach ($this->groupby as $prop)
+			$match->$prop = null;
+		$match->count = 0;
+		return $match;
+	}
+
+	public function count() {
+		$match = $this->_filter();
+
+		if (count($this->groupby) > 0) {
+
+			usort($match, array($this, "_cmp2"));
+
+			$last_match = $this->reset_match();
+
+			$result = array();
+			foreach ($match as $row) {
+				foreach ($this->groupby as $prop) {
+					if ($row->$prop != $last_match->$prop) {
+						if ($last_match->count > 0)
+							$result[] = $last_match;
+						$last_match = $this->reset_match();
+						break;
+					}
+				}
+				if ($last_match->count == 0) {
+					foreach ($this->groupby as $prop)
+						$last_match->$prop = $row->$prop;
+				}
+				$last_match->count++;
+			}
+			if ($last_match->count > 0)
+				$result[] = $last_match;
+
+			return $result;
+		}
+		else
+			return count($match);
+	}
+
+	public function sum() {
+		$attrs = func_get_args();
+
+		$match = $this->_filter();
+
+		if (count($this->groupby) > 0) {
+
+			usort($match, array($this, "_cmp2"));
+
+			$last_match = $this->reset_match();
+
+			$result = array();
+			foreach ($match as $row) {
+				foreach ($this->groupby as $prop) {
+					if ($row->$prop != $last_match->$prop) {
+						if ($last_match->count > 0)
+							$result[] = $last_match;
+						$last_match = $this->reset_match();
+						break;
+					}
+				}
+				if ($last_match->count == 0) {
+					foreach ($this->groupby as $prop)
+						$last_match->$prop = $row->$prop;
+				}
+				$last_match->count++;
+				foreach ($attrs as $attr) {
+					$prop = "sum_" . $attr;
+					if (!isset($last_match->$prop))
+						$last_match->$prop = 0;
+					$last_match->$prop += $row->$attr;
+				}
+			}
+			if ($last_match->count > 0)
+				$result[] = $last_match;
+
+			return $result;
+		}
+		else
+			return array_sum($match);
 	}
 }
